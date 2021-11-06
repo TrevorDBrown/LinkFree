b277396582e09a4766007823861aa787338bc9c5
TrevorDBrown <trevordylanbrown@gmail.com>
2021-11-06 00:02:53 -0500

    Created app.js
    - Created entrypoint for application. Serves the link page via Express.js.

---
+++ src/app.js
@@ -0,0 +1,30 @@
+/*
+*   LinkFreePlus - an open source interpretation of Linktree.
+*   (c)2021 Trevor D. Brown. Distributed under MIT License.
+*   Original Linkfree source: (c)2019-2021 MichaelBarney. Distributed under Apache License 2.0.
+*
+*   app.js - application entry point.
+*/
+
+const express = require('express');
+const app = express();
+
+const linkfree = require('./linkfree');
+
+// Endpoints
+app.get('/', (req, res) => {
+    linkfree.generateLinkPage((error, pageContent) => {
+        if (!error){
+            res.status(200).send(pageContent);
+        }else{
+            res.status(500).send(pageContent);
+        }
+    });
+});
+
+// Port for server (defined in environmental variable LF_PORT, or default to port 3000)
+const port = process.env.LF_PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Linkfree instance running at: http://localhost:${port}`);
+});
